06b4caa6d4866f109a0ac90d3609766a07bb49b9
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-08-19 22:55:07 +0100

    config

---
+++ config/__init__.py
@@ -0,0 +1,34 @@
+import os
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+# uri = f"""postgresql://{os.environ.get('POSTGRES_USER')}:{os.environ.get('POSTGRES_PASSWORD')}@{os.environ.get('POSTGRES_HOST')}:{os.environ.get('POSTGRES_PORT')}/{os.environ.get('POSTGRES_DB')}"""
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # jtw config
+    JWT_SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    # expiring time
+    JWT_ACCESS_TOKEN_EXPIRES = 3600
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir, 'iszify.sqlite')
+    # SQLALCHEMY_DATABASE_URI = uri
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = "sqlite://"
+
+
+config_obj = {
+    'development': DevelopmentConfig,
+    'testing': TestConfig
+}
