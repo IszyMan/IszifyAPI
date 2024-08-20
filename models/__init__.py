f71978af71dd487229098be2a17da8258a42dc4b
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-08-20 14:19:50 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
 from .users import (authenticate, create_user, get_user,
                     create_reset_p, get_user_by_reset_p,
-                    change_password, create_otp)
+                    change_password, create_otp, current_user_info)
