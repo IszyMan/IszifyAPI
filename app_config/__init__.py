857b8bcdaf506227a03833ac2c862763d98fbf13
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-08-20 10:25:21 +0100

    Update __init__.py

--- app_config/__init__.py
+++ app_config/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from extensions import db, jwt, migrate, mail
 from config import config_obj
+from endpoints import AuthenticationBlueprint
 
 
 def create_app(config_name="development"):
@@ -12,5 +13,7 @@ def create_app(config_name="development"):
     jwt.init_app(app)
     migrate.init_app(app, db)
     mail.init_app(app)
+
+    app.register_blueprint(AuthenticationBlueprint, url_prefix="/api/v1")
 
     return app
