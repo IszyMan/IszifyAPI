a6c7bcc53e931b2588e620f271023f79c6142117
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-08-20 14:19:29 +0100

    Update __init__.py

--- app_config/__init__.py
+++ app_config/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
-from extensions import db, jwt, migrate, mail
+from extensions import db, jwt, migrate, mail, cors
 from config import config_obj
-from endpoints import AuthenticationBlueprint
+from models.users import Users
+from models.shorten_url import Urlshort, UrlShortenerClicks, ShortUrlClickLocation
+from endpoints import AuthenticationBlueprint, UserBlueprint
+from utils import return_response
+from http_status import HttpStatus
+from status_res import StatusRes
 
 
 def create_app(config_name="development"):
@@ -12,8 +17,40 @@ def create_app(config_name="development"):
     db.init_app(app)
     jwt.init_app(app)
     migrate.init_app(app, db)
+    cors.init_app(app, resources={r"/api/*": {"origins": "*"}})
     mail.init_app(app)
 
+    # user loader
+    @jwt.user_lookup_loader
+    def user_lookup_callback(_jwt_header, jwt_data):
+        user_id = jwt_data["sub"]
+        return Users.query.get(user_id)
+
+    @jwt.expired_token_loader
+    def my_expired_token_callback(jwt_header, jwt_payload):
+        return return_response(
+            HttpStatus.UNAUTHORIZED,
+            status=StatusRes.FAILED,
+            message="Token Expired",
+        )
+
+    @jwt.invalid_token_loader
+    def my_invalid_token_callback(jwt_header, jwt_payload):
+        return return_response(
+            HttpStatus.UNAUTHORIZED,
+            status=StatusRes.FAILED,
+            message="Invalid Token",
+        )
+
+    @jwt.unauthorized_loader
+    def my_unauthorized_callback(jwt_header, jwt_payload):
+        return return_response(
+            HttpStatus.UNAUTHORIZED,
+            status=StatusRes.FAILED,
+            message="Unauthorized",
+        )
+
     app.register_blueprint(AuthenticationBlueprint, url_prefix="/api/v1")
+    app.register_blueprint(UserBlueprint, url_prefix="/api/v1")
 
     return app
