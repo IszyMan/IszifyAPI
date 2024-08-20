b42f26e8a152d6e146854e1b78bd1bde0ffb683b
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-08-20 18:13:48 +0100

    Update user_account.py

--- endpoints/user_account.py
+++ endpoints/user_account.py
@@ -38,81 +38,81 @@ def dashboard():
         )
 
 
-@user_blp.route("/short_url/create", methods=["POST"])
-@jwt_required()
-@email_verified
-def shorten_url():
-    try:
-        print("got hereeeee")
-        data = request.get_json()
-        original_url = data.get("original_url")
-        custom_url = data.get("custom_url")
-        want_qr_code = data.get("want_qr_code", False)
-
-        title = data.get("title", f"Untitled {datetime.now().strftime('%Y-%m-%d %I:%M:%S %Z ')}")
-
-        print(
-            f"original_url: {original_url}, custom_url: {custom_url}, title: {title}, "
-            f"want_qr_code: {want_qr_code}"
-        )
-
-        if Urlshort.query.filter_by(
-                user_id=current_user.id, url=original_url
-        ).first():
-            return return_response(
-                HttpStatus.CONFLICT,
-                status=StatusRes.FAILED,
-                message="URL already exists, please filter by the url to get the details",
-            )
-        if not validate_url(original_url):
-            return return_response(
-                HttpStatus.BAD_REQUEST,
-                status=StatusRes.FAILED,
-                message="Invalid URL",
-            )
-
-        if custom_url:
-            print(custom_url, "custom_url")
-            short_url = custom_url
-            if Urlshort.query.filter_by(short_url=short_url).first():
-                return return_response(
-                    HttpStatus.CONFLICT,
-                    status=StatusRes.FAILED,
-                    message="URL already exists",
-                )
-        else:
-            short_url = generate_short_url()
-
-        url_and_short_url = f"{request.host_url}{short_url}"
-
-        # res = generate_and_save_qr(url_and_short_url) if generate_qr_code else ""
-        # print(res, 'res')
-
-        url = Urlshort(
-            user_id=current_user.id,
-            url=original_url,
-            short_url=short_url,
-            title=title,
-            want_qr_code=want_qr_code,
-        )
-        url.save()
-
-        return return_response(
-            HttpStatus.OK,
-            status=StatusRes.SUCCESS,
-            message="Short URL created successfully",
-        )
-
-    except Exception as e:
-        print(traceback.format_exc(), "traceback@user_blp/shorten_url")
-        print(e, "error@user_blp/shorten_url")
-        db.session.rollback()
-        return return_response(
-            HttpStatus.INTERNAL_SERVER_ERROR,
-            status=StatusRes.FAILED,
-            message="Network Error",
-        )
-
+# @user_blp.route(f"{USER_PREFIX}/short_url/create", methods=["POST"])
+# @jwt_required()
+# @email_verified
+# def shorten_url():
+#     try:
+#         print("got hereeeee")
+#         data = request.get_json()
+#         original_url = data.get("original_url")
+#         custom_url = data.get("custom_url")
+#         want_qr_code = data.get("want_qr_code", False)
+#
+#         title = data.get("title", f"Untitled {datetime.now().strftime('%Y-%m-%d %I:%M:%S %Z ')}")
+#
+#         print(
+#             f"original_url: {original_url}, custom_url: {custom_url}, title: {title}, "
+#             f"want_qr_code: {want_qr_code}"
+#         )
+#
+#         if Urlshort.query.filter_by(
+#                 user_id=current_user.id, url=original_url
+#         ).first():
+#             return return_response(
+#                 HttpStatus.CONFLICT,
+#                 status=StatusRes.FAILED,
+#                 message="URL already exists, please filter by the url to get the details",
+#             )
+#         if not validate_url(original_url):
+#             return return_response(
+#                 HttpStatus.BAD_REQUEST,
+#                 status=StatusRes.FAILED,
+#                 message="Invalid URL",
+#             )
+#
+#         if custom_url:
+#             print(custom_url, "custom_url")
+#             short_url = custom_url
+#             if Urlshort.query.filter_by(short_url=short_url).first():
+#                 return return_response(
+#                     HttpStatus.CONFLICT,
+#                     status=StatusRes.FAILED,
+#                     message="URL already exists",
+#                 )
+#         else:
+#             short_url = generate_short_url()
+#
+#         url_and_short_url = f"{request.host_url}{short_url}"
+#
+#         # res = generate_and_save_qr(url_and_short_url) if generate_qr_code else ""
+#         # print(res, 'res')
+#
+#         url = Urlshort(
+#             user_id=current_user.id,
+#             url=original_url,
+#             short_url=short_url,
+#             title=title,
+#             want_qr_code=want_qr_code,
+#         )
+#         url.save()
+#
+#         return return_response(
+#             HttpStatus.OK,
+#             status=StatusRes.SUCCESS,
+#             message="Short URL created successfully",
+#         )
+#
+#     except Exception as e:
+#         print(traceback.format_exc(), "traceback@user_blp/shorten_url")
+#         print(e, "error@user_blp/shorten_url")
+#         db.session.rollback()
+#         return return_response(
+#             HttpStatus.INTERNAL_SERVER_ERROR,
+#             status=StatusRes.FAILED,
+#             message="Network Error",
+#         )
+#
 
 # redirect short url to the original url
 @user_blp.route("/<short_url>/")
