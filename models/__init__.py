e65092619243e85bfc9c633a408f76f77e2e0141
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-08-24 20:37:48 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from .users import (authenticate, create_user, get_user,
                     create_reset_p, get_user_by_reset_p,
                     change_password, create_otp, current_user_info)
 from .shorten_url import generate_short_url, Urlshort, validate_url, save_url_clicks
+from .blogs import Catgories, Blogs, save_blog, get_blogs_per_category, get_categories, get_blog, get_all_blogs
