19005164913c203ccb9bc1d0599d4d1d1f9c3d12
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-08-26 15:01:40 +0100

    Update __init__.py

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -1,3 +1,4 @@
 from .auth import auth_blp as AuthenticationBlueprint
 from .user_account import user_blp as UserBlueprint
 from .blog import blog_blp as BlogBlueprint
+from .qr_code import qrcode_blp as QRCodeBlueprint
