c87ad61629f7244d5a49d336bd049a26476f56bb
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-09-01 12:32:08 +0100

    Update qr_code.py

--- endpoints/qr_code.py
+++ endpoints/qr_code.py
@@ -6,6 +6,8 @@ from models import (
     get_qrcode_categories,
     save_qrcode_data,
     get_qrcode_data,
+    update_qrcode_data,
+    get_qrcode_data_by_id
 )
 from extensions import db
 from utils import (
@@ -14,6 +16,7 @@ from utils import (
 import traceback
 from flask_jwt_extended import jwt_required, current_user
 from datetime import datetime
+import pprint
 
 AUTH_PREFIX = "qr_code"
 
@@ -63,6 +66,8 @@ def qrcode():
     try:
         if request.method == "POST":
             data = request.get_json()
+
+            pprint.pprint(data)
 
             category = data.get("category")
             social_media = data.get("social_media")
@@ -164,3 +169,96 @@ def qrcode():
 
 
 # edit qr code
+@qrcode_blp.route(f"/{AUTH_PREFIX}/qrcode/<qr_code_id>", methods=["GET", "PATCH"])
+@jwt_required()
+def edit_qrcode(qr_code_id):
+    try:
+        if request.method == "PATCH":
+            data = request.get_json()
+
+            pprint.pprint(data)
+
+            if not qr_code_id:
+                return return_response(
+                    HttpStatus.BAD_REQUEST,
+                    status=StatusRes.FAILED,
+                    message="QR Code ID is required",
+                )
+
+            social_media = data.get("social_media")
+            qr_style = data.get("qr_style")
+            if social_media and not isinstance(social_media, list):
+                return return_response(
+                    HttpStatus.BAD_REQUEST,
+                    status=StatusRes.FAILED,
+                    message="Social Media must be an array",
+                )
+
+            if qr_style and not isinstance(qr_style, list):
+                return return_response(
+                    HttpStatus.BAD_REQUEST,
+                    status=StatusRes.FAILED,
+                    message="QR Style must be an array",
+                )
+
+            payload = dict(
+                url=data.get("url"),
+                phone_number=data.get("phone_number"),
+                message=data.get("message"),
+                email=data.get("email"),
+                subject=data.get("subject"),
+                ssid=data.get("ssid"),
+                password=data.get("password"),
+                encryption=data.get("encryption"),
+                ios_url=data.get("ios_url"),
+                android_url=data.get("android_url"),
+                other_device_url=data.get("other_device_url"),
+                longitude=data.get("longitude"),
+                latitude=data.get("latitude"),
+                trade_number=data.get("trade_number"),
+                prefix=data.get("prefix"),
+                first_name=data.get("first_name"),
+                last_name=data.get("last_name"),
+                company_name=data.get("company_name"),
+                mobile_phone=data.get("mobile_phone"),
+                fax=data.get("fax"),
+                postal_code=data.get("postal_code"),
+                religion=data.get("religion"),
+                street=data.get("street"),
+                city=data.get("city"),
+                state=data.get("state"),
+                country=data.get("country"),
+            )
+
+            res = update_qrcode_data(payload, current_user.id, qr_code_id)
+
+            if not res:
+                return return_response(
+                    HttpStatus.NOT_FOUND,
+                    status=StatusRes.FAILED,
+                    message="QR Code not found",
+                )
+
+            return return_response(
+                HttpStatus.OK, status=StatusRes.SUCCESS,
+                message="QR Code Updated"
+            )
+
+        # get qr code
+        qr_code = get_qrcode_data_by_id(qr_code_id, current_user.id)
+        return return_response(
+            HttpStatus.OK,
+            status=StatusRes.SUCCESS,
+            message="QR Code",
+            **qr_code
+        )
+
+    except Exception as e:
+        print(traceback.format_exc(), "traceback@qrcode_blp/edit_qrcode")
+        print(e, "error@qrcode_blp/edit_qrcode")
+        db.session.rollback()
+        return return_response(
+            HttpStatus.INTERNAL_SERVER_ERROR,
+            status=StatusRes.FAILED,
+            message="Network Error",
+        )
