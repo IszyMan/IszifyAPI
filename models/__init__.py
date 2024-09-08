0a51aa03786b6c55735eb4fc8600ecb7bde7bd8d
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-09-08 11:49:07 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,31 @@
-from .users import (authenticate, create_user, get_user,
-                    create_reset_p, get_user_by_reset_p,
-                    change_password, create_otp, current_user_info)
+from .users import (
+    authenticate,
+    create_user,
+    get_user,
+    create_reset_p,
+    get_user_by_reset_p,
+    change_password,
+    create_otp,
+    current_user_info,
+)
 from .shorten_url import generate_short_url, Urlshort, validate_url, save_url_clicks
-from .blogs import (Catgories, Blogs, save_blog, get_blogs_per_category, get_categories, get_blog,
-                    get_all_blogs, save_category, category_exists)
-from .qrcode import (QRCodeCategories, save_qrcode_category,
-                     get_qrcode_categories, save_qrcode_data, get_qrcode_data,
-                     update_qrcode_data, get_qrcode_data_by_id)
+from .blogs import (
+    Catgories,
+    Blogs,
+    save_blog,
+    get_blogs_per_category,
+    get_categories,
+    get_blog,
+    get_all_blogs,
+    save_category,
+    category_exists,
+)
+from .qrcode import (
+    QRCodeCategories,
+    save_qrcode_category,
+    get_qrcode_categories,
+    save_qrcode_data,
+    get_qrcode_data,
+    update_qrcode_data,
+    get_qrcode_data_by_id,
+)
