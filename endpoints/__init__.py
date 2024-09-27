fead6be4493f7ab296fd84b19bb808cae8f24ea1
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-09-27 22:53:46 +0100

    Update __init__.py

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -2,3 +2,4 @@ from .auth import auth_blp as AuthenticationBlueprint
 from .user_account import user_blp as UserBlueprint
 from .blog import blog_blp as BlogBlueprint
 from .qr_code import qrcode_blp as QRCodeBlueprint
+from .redirect_url import redirect_url_blp as RedirectUrlBlueprint
