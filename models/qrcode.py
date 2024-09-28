da777838adc2f44b0c10e59671ad5d8d0f66f697
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-09-28 17:22:42 +0100

    Update qrcode.py

--- models/qrcode.py
+++ models/qrcode.py
@@ -349,14 +349,10 @@ def update_qrcode_data(qrcode_data_payload, user_id, qr_id):
 
 
 def get_qrcode_data_by_id(user_id, qr_id, fetch_type=None):
-    print("user_id: ", user_id, "qr_id: ", qr_id)
-    qrcode_data = QRCodeData.query.filter(
-        QRCodeData.user_id == user_id, QRCodeData.id == qr_id
-    ).first()
-
-    if not fetch_type:
-        return qrcode_data.to_dict() if qrcode_data else None
-    return qrcode_data if qrcode_data else None
+    qrcode_data = QRCodeData.query.filter_by(user_id=user_id, id=qr_id).first()
+    if not qrcode_data:
+        return None
+    return qrcode_data if fetch_type else qrcode_data.to_dict()
 
 
 def qrcode_styling(payload, qrcode_id):
