a763002d3f24ce9a947b44d1c4690fecbbe5e5bc
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-10-01 20:41:41 +0100

    Update qrcode.py

--- models/qrcode.py
+++ models/qrcode.py
@@ -377,8 +377,8 @@ def get_qrcode_data_by_id(user_id, qr_id, fetch_type=None):
 
 
 @retry_on_exception(retries=3, delay=1)
-def qrcode_styling(payload, qrcode_id):
-    existing_style = QrCodeStyling.query.filter_by(qrcode_id=qrcode_id).first()
+def qrcode_styling(payload, qrcode_id, user_id):
+    existing_style = QrCodeStyling.query.filter_by(qrcode_id=qrcode_id, user_id=user_id).first()
     if existing_style:
         print("update existing style")
         existing_style.width = payload.get("width", existing_style.width)
