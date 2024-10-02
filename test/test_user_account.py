58dc8eed93ae5aafafafd53c728255be23faad5e
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-10-02 16:00:34 +0100

    Update test_user_account.py

--- test/test_user_account.py
+++ test/test_user_account.py
@@ -32,9 +32,7 @@ class TestUser(unittest.TestCase):
         self.access_token = return_access_token(user)
         print(self.access_token, "access token")
 
-        self.headers = {
-            "Authorization": f"Bearer {self.access_token}"
-        }
+        self.headers = {"Authorization": f"Bearer {self.access_token}"}
 
     def tearDown(self):
         db.session.remove()
@@ -45,18 +43,25 @@ class TestUser(unittest.TestCase):
         res = self.client.get("/api/v1/user/dashboard", headers=self.headers)
         self.assertEqual(res.status_code, 200)
 
-    def test_short_url(self): # comment out this line "request.urlopen(url)" in ahorten_url.py file in models dir, the functions that validates the url
+    def test_short_url(
+        self,
+    ):  # comment out this line "request.urlopen(url)" in ahorten_url.py file in models dir, the functions that
+        # validates the url
         data = {
             "original_url": "https://www.mygreatlearning.com/blog/top-django-projects/#hospital-management-system"
         }
-        res = self.client.post("/api/v1/user/short_url/create", json=data, headers=self.headers)
+        res = self.client.post(
+            "/api/v1/user/short_url/create", json=data, headers=self.headers
+        )
         self.assertEqual(res.status_code, 201)
 
     def test_create_qr_code(self):
         data = {
             "data": "https://www.mygreatlearning.com/blog/top-django-projects/#hospital-management-system",
-            "category": "url"
+            "category": "url",
         }
-        res = self.client.post("/api/v1/qr_code/qrcode", json=data, headers=self.headers)
+        res = self.client.post(
+            "/api/v1/qr_code/qrcode", json=data, headers=self.headers
+        )
         print(res.json, "res.json")
         self.assertEqual(res.status_code, 201)
