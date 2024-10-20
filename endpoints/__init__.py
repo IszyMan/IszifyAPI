55536d6a8ee9f26b8956d019daf0921ac609f4ae
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-10-20 21:45:35 +0100

    import thr blueprint

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -3,3 +3,4 @@ from .user_account import user_blp as UserBlueprint
 from .blog import blog_blp as BlogBlueprint
 from .qr_code import qrcode_blp as QRCodeBlueprint
 from .redirect_url import redirect_url_blp as RedirectUrlBlueprint
+from .shorten_url import url_short_blp as UrlShortBlueprint
