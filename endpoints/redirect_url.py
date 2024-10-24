b3dc6915624f4da5b02ca3bf18e94ee1f96ce48f
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-10-24 17:28:11 +0100

    Update redirect_url.py

--- endpoints/redirect_url.py
+++ endpoints/redirect_url.py
@@ -1,15 +1,17 @@
 from flask import Blueprint, redirect
-from models import get_original_url_by_short_url, get_url_by_short_url
+from models import get_original_url_by_short_url, get_url_by_short_url, get_unauth_url_by_short_url
+import os
 
 
 redirect_url_blp = Blueprint("redirect_url_blp", __name__)
 
-DEFAULT_REDIRECT_URL = "https://www.google.com"
+DEFAULT_REDIRECT_URL = os.environ.get("DEFAULT_REDIRECT_URL")
+# print("DEFAULT_REDIRECT_URL", DEFAULT_REDIRECT_URL)
 
 
 @redirect_url_blp.route("/<short_url>", methods=["GET"])
 def redirect_url(short_url):
-    url = get_url_by_short_url(short_url) or get_original_url_by_short_url(short_url)
+    url = get_url_by_short_url(short_url) or get_original_url_by_short_url(short_url) or get_unauth_url_by_short_url(short_url)
 
     # Redirect to the found URL or the default URL if not found
     return redirect(url if url else DEFAULT_REDIRECT_URL)
