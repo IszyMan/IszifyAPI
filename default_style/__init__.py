38c0a80a1090834b14ac4208fb8d36fb6de5f7b1
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-10-24 17:28:01 +0100

    Create __init__.py

---
+++ default_style/__init__.py
@@ -0,0 +1,12 @@
+def return_default_style():
+    return {
+        "width": 200,
+        "height": 200,
+        "margin": 0,
+        "qrOptions": {"typeNumber": "5", "mode": "Byte", "errorCorrectionLevel": "Q"},
+        "imageOptions": {"hideBackgroundDots": True, "imageSize": 0.4, "margin": 0},
+        "dotsOptions": {"type": "classy-rounded", "color": "#000000", "gradient": ""},
+        "backgroundOptions": {"color": "#FFFFFF"},
+        "cornersSquareOptions": {"type": "extra-rounded", "color": "#000000"},
+        "cornersDotOptions": {"type": "", "color": "#000000"},
+    }
