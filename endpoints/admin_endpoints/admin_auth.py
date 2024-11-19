d8745832f1826d083347c21be6e6df34bad3626c
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-11-19 11:29:23 +0100

    Create admin_auth.py

---
+++ endpoints/admin_endpoints/admin_auth.py
@@ -0,0 +1,57 @@
+from flask import Blueprint, request
+from http_status import HttpStatus
+from status_res import StatusRes
+from models import (
+    admin_authenticate,
+)
+from extensions import db, limiter
+from utils import (
+    return_response,
+    return_access_token,
+)
+import traceback
+from flask_jwt_extended import current_user, jwt_required
+from passlib.hash import pbkdf2_sha256 as hasher
+
+USER_PREFIX = "admin_auth"
+
+admin_auth_blp = Blueprint("admin_auth_blp", __name__)
+
+
+@admin_auth_blp.route(f"/{USER_PREFIX}/login", methods=["POST"])
+def admin_login():
+    try:
+        data = request.get_json()
+        email = data.get("email")
+        password = data.get("password")
+
+        print(email, password)
+
+        if not email or not password:
+            return return_response(
+                HttpStatus.BAD_REQUEST,
+                status=StatusRes.FAILED,
+                message="Email and Password are required",
+            )
+        admin = admin_authenticate(email.lower(), password)
+        if admin:
+            return return_response(
+                HttpStatus.OK,
+                status=StatusRes.SUCCESS,
+                message="Login Successful",
+                access_token=return_access_token(admin),
+            )
+        return return_response(
+            HttpStatus.NOT_FOUND,
+            status=StatusRes.FAILED,
+            message="Invalid Email or Password",
+        )
+    except Exception as e:
+        print(traceback.format_exc(), "traceback@user_blp/admin_login")
+        print(e, "error@user_blp/admin_login")
+        db.session.rollback()
+        return return_response(
+            HttpStatus.INTERNAL_SERVER_ERROR,
+            status=StatusRes.FAILED,
+            message="Network Error",
+        )
