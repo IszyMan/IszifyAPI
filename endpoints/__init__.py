22997d9b6f08f03192b821b56ee0e42c777a3918
ADENIYI OLANREWAJU MARK <adeniyiboladale@yahoo.com>
2024-11-19 11:29:15 +0100

    Update __init__.py

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -5,3 +5,5 @@ from .qr_code import qrcode_blp as QRCodeBlueprint
 from .redirect_url import redirect_url_blp as RedirectUrlBlueprint
 from .shorten_url import url_short_blp as UrlShortBlueprint
 from .qr_code_unauth_users import unauth_qrcode_blp as UnauthQRCodeBlueprint
+from .admin_endpoints import admin_auth_blp as AdminAuthBlueprint
+from .admin_endpoints import admin_blp as AdminAccountBlueprint
