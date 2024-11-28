cb5f10d4869af1a8ec2236ef0c3127b98962b923
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-11-28 21:45:03 +0100

    Create services.py

---
+++ api_services/paystack/services.py
@@ -0,0 +1,13 @@
+from .base import PaystackBase
+import requests
+
+class PaystackClient(PaystackBase):
+    def get_all_banks(self):
+        try:
+            url = f"{self.base_url}/bank?country={self.country}"
+            response = requests.get(url, headers=self.headers)
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            print("error", e)
+            return None
