19d0165e07c1440c6120618f9443efd5bd873cc3
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-11-29 21:49:30 +0100

    Update transactions_views.py

--- endpoints/transactions/transactions_views.py
+++ endpoints/transactions/transactions_views.py
@@ -4,13 +4,28 @@ from utils import return_response
 import traceback
 from status_res import StatusRes
 from http_status import HttpStatus
+from . import pay_stack
 
 transactions_blp = Blueprint("transactions_blp", __name__)
 
-@transactions_blp.route("/get_banks", methods=["GET"])
+TRANSACT_PREFIX = "transactions"
+
+@transactions_blp.route(f"{TRANSACT_PREFIX}/get_banks", methods=["GET"])
 def list_banks():
     try:
-        pass
+        res = pay_stack.get_banks()
+        if not res:
+            return return_response(
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                status=StatusRes.FAILED,
+                message="Failed to get banks",
+            )
+        return return_response(
+            HttpStatus.OK,
+            status=StatusRes.SUCCESS,
+            message="Banks Retrieved",
+            data=res
+        )
     except Exception as e:
         print(traceback.format_exc(), "traceback@transactions_blp/list_banks")
         print(e, "error@transactions_blp/list_banks")
