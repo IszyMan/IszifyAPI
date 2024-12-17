a361ad688e6db5585dd7581886e36d08570c874c
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-12-17 04:25:01 +0100

    Update payment.py

--- models/payment.py
+++ models/payment.py
@@ -1,5 +1,6 @@
 from extensions import db
 from func import hex_id
+from datetime import datetime, timedelta
 
 
 class PaymentPlans(db.Model):
@@ -91,7 +92,7 @@ def create_payment_plan(name, amount, currency, duration):
     return plan
 
 def get_payment_plans():
-    plans = PaymentPlans.query.all()
+    plans = PaymentPlans.query.order_by(PaymentPlans.amount.asc()).all()
     return [plan.to_dict() for plan in plans]
 
 
@@ -123,3 +124,59 @@ def delete_payment_plan(plan_id):
     db.session.delete(plan)
     db.session.commit()
     return True, plan_name
+
+
+# subscribe to a payment plan
+def subscribe(user_id, plan_id, status):
+    plan = PaymentPlans.query.filter_by(id=plan_id).first()
+    if not plan:
+        return False
+    start_date = datetime.now()
+    end_date = start_date + timedelta(days=plan.duration)
+    sub = Subscriptions(user_id, plan_id, start_date, end_date, status)
+    sub.save()
+    return sub
+
+
+# get all subscriptions
+def get_all_subscriptions():
+    # lower amount should be first
+    subs = Subscriptions.query.order_by(Subscriptions.start_date.asc()).all()
+    return [sub.to_dict() for sub in subs]
+
+
+# get transactions and filters
+def get_transactions(page, per_page, user_id, reference=None, status=None, start_date=None, end_date=None):
+    query = Transactions.query.filter(Transactions.user_id == user_id)
+
+    # Filter by start_date and end_date if provided
+    if start_date:
+        query = query.filter(Transactions.date >= start_date)
+    if end_date:
+        query = query.filter(Transactions.date <= end_date)
+
+    # Filter by reference if provided
+    if reference:
+        query = query.filter(Transactions.transaction_reference.ilike(f"%{reference}%"))
+
+    # Filter by status if provided
+    if status:
+        query = query.filter(Transactions.status == status)
+
+    # Order by creation date descending and paginate
+    transactions = query.order_by(Transactions.date.desc()).paginate(
+        page=page, per_page=per_page, error_out=False
+    )
+
+    return {
+        "data": [transaction.to_dict() for transaction in transactions.items],
+        "total_items": transactions.total,
+        "page": transactions.page,
+        "total_pages": transactions.pages,
+        "per_page": transactions.per_page,
+    }
+
+# get one transaction
+def get_one_transaction(transaction_id, user_id):
+    transaction = Transactions.query.filter_by(id=transaction_id, user_id=user_id).first()
+    return transaction.to_dict() if transaction else None
