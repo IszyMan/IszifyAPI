b4fb52595fff05f8626589149e7bd01caf63dfdd
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2024-12-22 12:00:38 +0100

    Create bio_link.py

---
+++ models/bio_link.py
@@ -0,0 +1,30 @@
+from extensions import db
+from func import hex_id
+from sqlalchemy import func
+from datetime import datetime, timedelta
+
+
+class BioLink(db.Model):
+    __tablename__ = "bio_link"
+
+    id = db.Column(db.String(50), primary_key=True, default=hex_id)
+    created = db.Column(db.DateTime, nullable=False, default=db.func.now())
+    updated = db.Column(db.DateTime, nullable=False, default=db.func.now(), onupdate=db.func.now())
+
+
+    def __repr__(self):
+        return f"<BioLink {self.id}>"
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+
+# save bio link
+# def save_bio_link(bio_link):
+#     db.session.add(bio_link)
+#     db.session.commit()
+#     return True
