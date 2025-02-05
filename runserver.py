f47af7d4481a3f237204115b32123310ad9139c1
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-02-05 13:03:07 +0100

    Update runserver.py

--- runserver.py
+++ runserver.py
@@ -1,6 +1,9 @@
 from app_config import create_app
 from dotenv import load_dotenv
 
+# from models import Users, subscribe_for_beginner
+# from logger import logger
+
 
 load_dotenv()
 
@@ -9,4 +12,10 @@ app = create_app()
 
 if __name__ == "__main__":
     # gunicorn -w 4 -b 0.0.0.0:7000 'app_config:create_app()'
+    # subscribe for existing users
+    # with app.app_context():
+    #     for user in Users.query.all():
+    #         if not user.subscriptions:
+    #             logger.info(f"Subscribing user {user.id}")
+    #             subscribe_for_beginner(user.id)
     app.run(host="0.0.0.0", port=7000)
