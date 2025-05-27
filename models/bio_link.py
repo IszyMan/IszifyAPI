4891dadff55ba8382af3dd40afa03ca26642c173
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-05-27 15:41:13 +0100

    Update bio_link.py

--- models/bio_link.py
+++ models/bio_link.py
@@ -23,21 +23,3 @@ class BioLink(db.Model):
 
     def update(self):
         db.session.commit()
-
-
-# save bio link
-# def save_bio_link(bio_link):
-#     db.session.add(bio_link)
-#     db.session.commit()
-#     return True
-
-
-def get_current_bio_link_count(current_user):
-    """Return the current count of BioLink records."""
-    now = datetime.utcnow()
-    return (
-        BioLink.query.filter_by(user=current_user)
-        .filter(extract("year", BioLink.created) == now.year)
-        .filter(extract("month", BioLink.created) == now.month)
-        .count()
-    )
