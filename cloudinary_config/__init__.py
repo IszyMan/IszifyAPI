35d996fa4d9e8bf9f500e01554005896338940c7
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-06-02 22:36:54 +0100

    Create __init__.py

---
+++ cloudinary_config/__init__.py
@@ -0,0 +1,13 @@
+import cloudinary
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+cloud_name = os.environ.get("CLOUDINARY_CLOUD_NAME")
+api_key = os.environ.get("CLOUDINARY_API_KEY")
+api_secret = os.environ.get("CLOUDINARY_API_SECRET")
+
+cloudinary.config(
+    cloud_name=cloud_name, api_key=api_key, api_secret=api_secret, secure=True
+)
