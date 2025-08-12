02cc1844af6bd26ac8a7ad9796ad4bd23b99906d
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-08-12 19:57:50 +0100

    Update services.py

--- api_services/paystack/services.py
+++ api_services/paystack/services.py
@@ -14,6 +14,34 @@ class PaystackClient(PaystackBase):
             logger.error(f"error {e}")
             return None
 
+    def resolve_account(self, account_number, bank_code):
+        try:
+            print(self.headers, "header from resolve_account")
+            url = f"{self.base_url}/bank/resolve?account_number={account_number}&bank_code={bank_code}"
+            response = requests.get(url, headers=self.headers)
+            response.raise_for_status()  # This will raise an HTTPError for bad responses (4xx and 5xx)
+
+            # Debugging statements
+            print(response.content.decode(), "response content from resolve_account")
+            print(response.json(), "response JSON from resolve_account")
+
+            return response.json(), response.status_code
+
+        except requests.exceptions.HTTPError as http_err:
+            # Handles HTTP errors (4xx, 5xx)
+            print(f"HTTP error occurred: {http_err}")
+            return {}, 500
+
+        except requests.exceptions.RequestException as req_err:
+            # Handles other requests exceptions (e.g., connection errors)
+            print(f"Request exception occurred: {req_err}")
+            return {}, 500
+
+        except Exception as e:
+            # Handles all other exceptions
+            print(f"An error occurred: {e}")
+            return {}, 500
+
     # verify transaction
     def verify_transaction(self, reference):
         try:
