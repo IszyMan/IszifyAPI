b36d6437de164768148ac16cf90745eb28534201
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2025-12-19 09:19:55 +0100

    Update celery_works.py

--- celery_config/utils/celery_works.py
+++ celery_config/utils/celery_works.py
@@ -1,8 +1,9 @@
 from celery_config.settings import celery, shared_task
 from models.shorten_url import Urlshort
 from models.qrcode import QRCodeData
-from crud import save_qrcode_clicks, save_url_clicks
+from crud import save_qrcode_clicks, save_url_clicks, save_transactions, save_donation
 from extensions import db
+from logger import logger
 
 
 @shared_task
@@ -29,3 +30,35 @@ def save_clicks_for_analytics(short_url, payload):
     print(url.url, "the real url")
 
     return True
+
+
+# SAVE FROM VERIFY TRANSACTIONS
+@shared_task
+def save_transaction_from_verify_transaction(
+    reference_number, amount, email, goal_id, name, message, res, user_id, trans_type
+):
+    try:
+        save_transactions(
+            user_id,
+            "",
+            amount,
+            "",
+            trans_type,
+            reference_number,
+            "",
+            "",
+            "",
+            "",
+            "success",
+            response_json=res,
+        )
+        save_donation(goal_id, name, amount, message, True, reference_number, email)
+    except Exception as e:
+        logger.exception(
+            "traceback@celery_works/save_transaction_from_verify_transaction"
+        )
+        logger.error(
+            f"{e}: error@celery_works/save_transaction_from_verify_transaction"
+        )
+        db.session.rollback()
+        return False
