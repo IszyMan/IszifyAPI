f06cfbdad4bc77825910d6b4939f5ba60a5eac9f
ADENIYI OLANREWAJU MARK <100721103+engrmarkk@users.noreply.github.com>
2025-12-21 21:21:54 +0100

    Update giftlink.py

--- endpoints/giftlink.py
+++ endpoints/giftlink.py
@@ -848,7 +848,7 @@ def supporters_histories():
             message="Supporters histories fetched successfully",
             data={
                 "total_supporters": total_supporters,
-                "supporters": [supporter.to_dict() for supporter in supporters.items],
+                "supporters": [supporter.support_dict() for supporter in supporters.items],
                 "total_items": supporters.total,
                 "total_pages": supporters.pages,
                 "page": page,
